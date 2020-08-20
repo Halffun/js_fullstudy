ab105b6a6c6ea7375611906f535035832786e67d
hufei <1521380841@qq.com>
2020-08-20 14:43:13 +0800

    typescript-learn

---
+++ js/TS/hello-ts/readme.md
@@ -0,0 +1,7 @@
+1. 安装 ts
+2. tsc --init
+3. 编辑tsconfig.json
+
+
+<!-- 原始类型 -->
+boolean, number, string, null, undefined, void, symbol, bigint
---
+++ js/TS/hello-ts/src/index.ts
@@ -0,0 +1,44 @@
+// function greeter(person: string) {
+//   return "hello" + person
+// }
+// const user = 'wn'
+// greeter(user)
+
+
+
+// 类型
+// let isLoading: boolean = false
+// isLoading = 1
+
+// let num = 6
+// num = '8'
+
+// let book: string = 'typeScript'
+
+
+// 空值
+// function warnUser(): void {
+
+// }
+
+// const a: void = undefined // 特例
+
+
+// let ab: any = undefined
+
+
+// let aa: undefined = undefined
+
+
+// let sym1:symbol = Symbol('key1')
+// let sym2 = Symbol('key1')
+
+
+// BigInt
+const max = BigInt(Number.MAX_SAFE_INTEGER)
+
+const max1 = max + 1n
+const max2 = max + 2n
+
+console.log(max1, max2);
+
