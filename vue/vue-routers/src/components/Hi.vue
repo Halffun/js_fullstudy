ddba8b1931316425105b563e4f1884aee115120e
hufei <1521380841@qq.com>
2020-08-25 12:05:40 +0800

    vue-router-learn

---
+++ vue/vue-routers/src/components/Hi.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="hello">
+    <h1>{{msg}}</h1>
+    <router-link :to="{name: 'hi1', params: {name: '乐怡'}}">hi 一</router-link>
+    <span style="display: inline-block; width: 100px"></span>
+    <router-link to="/hi/hi2">hi 二</router-link>
+    <span style="display: inline-block; width: 100px"></span>
+    <router-link to="/hi">返回</router-link>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hi',
+  data () {
+    return {
+      msg: 'Hi, I am hf'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
