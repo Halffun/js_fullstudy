bcb3ca9338b71ebbefe76d832e01ad37c26264b9
hufei <1521380841@qq.com>
2020-10-08 23:41:46 +0800

    子集

---
+++ leetcode/回溯算法/子集/index.js
@@ -0,0 +1,42 @@
+// 78. 子集  https://leetcode-cn.com/problems/subsets/
+// 给定一组不含重复元素的整数数组 nums，返回该数组所有可能的子集（幂集）。
+
+// 说明：解集不能包含重复的子集。
+
+// 示例:
+
+// 输入: nums = [1,2,3]
+// 输出:
+// [
+//   [3],
+//   [1],
+//   [2],
+//   [1,2,3],
+//   [1,3],
+//   [2,3],
+//   [1,2],
+//   []
+// ]
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsets = function(nums) {
+  let result = []
+  function backtrack(start, tmpPath) {
+    // [1, 2, 3]
+    // 本质：n叉树的遍历
+    result.push(tmpPath.slice(0))
+    for (let i = start; i < nums.length; i++) {
+      let num = nums[i]
+      tmpPath.push(num)
+      backtrack(i + 1, tmpPath)
+      tmpPath.pop()
+    }
+  }
+  backtrack(0, [])
+  return result
+};
+
+console.log(subsets([1, 2, 3]));
