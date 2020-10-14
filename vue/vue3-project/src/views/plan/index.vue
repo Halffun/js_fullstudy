7a5c9be1c02bdaf58c91cc3e19789ce0eeb5fa76
hufei <1521380841@qq.com>
2020-10-14 15:36:39 +0800

    vue3-project

--- vue/vue3-project/src/views/plan/index.vue
+++ vue/vue3-project/src/views/plan/index.vue
@@ -1,11 +1,25 @@
 <template>
   <div>
-    plan
+    <Create @handlePlan="handlePlan"></Create>
   </div>
 </template>
 
 <script>
-export default {};
+import Create from './create'
+export default {
+  components: {
+    Create
+  },
+  setup () {
+    const handlePlan = (plan) => {
+      console.log(plan);
+    }
+
+    return {
+      handlePlan
+    }
+  }
+};
 </script>
 
 <style>
