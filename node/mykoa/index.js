d17521eb1ab91384fcb21076d70af04eb50fd96c
hufei <1521380841@qq.com>
2020-10-24 23:26:10 +0800

    koa源码的实现

--- node/mykoa/index.js
+++ node/mykoa/index.js
@@ -17,10 +17,17 @@
 
 
 let Koa = require('./lib/application')
-let app = new Koa() 
+let app = new Koa()
 
-app.use((req, res) => {
-  res.end('hello')
+app.use((ctx) => {
+  // console.log(ctx.req.url);
+  // console.log(ctx.request.req.url);
+  // console.log(ctx.response.req.url);
+  // console.log(ctx.request.url);
+  // console.log(ctx.request.path);
+  // console.log(ctx.url);
+  // console.log(ctx.path);
+  ctx.body = 'hello world'
+  console.log(ctx.body);
 })
-
 app.listen(3000)
--- node/mykoa/lib/context.js
+++ node/mykoa/lib/context.js
@@ -1,5 +1,19 @@
 let proto = {}
 
+function defineGetter(prop, name) { // prop代表对象, name代表对象上的属性
+  proto.__defineGetter__(name, function () { // 每个对象都有 __defineGetter__ 方法
+    return this[prop][name] // url == this.request.url
+  }) 
+}
+function defineSetter(prop, name) {
+  proto.__defineSetter__(name, function (val) { 
+    this[prop][name] = val 
+  }) 
+}
 
+defineGetter('request', 'url')
+defineGetter('request', 'path')
+defineGetter('response', 'body')
+defineSetter('response', 'body')
 
 module.exports = proto
