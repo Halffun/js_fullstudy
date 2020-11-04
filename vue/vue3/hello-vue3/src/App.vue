e5b21ce025c2665fd901a786fb112a5e027b1c78
hufei <1521380841@qq.com>
2020-11-04 18:59:42 +0800

    hello-vue3

---
+++ vue/vue3/hello-vue3/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <hr>
+  <comdemo />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import comdemo from './components/com-demo.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    comdemo
+  }
+}
+</script>
---
+++ vue/vue3/hello-vue3/src/components/com-demo.vue
@@ -0,0 +1,37 @@
+<!-- fragment -->
+<template>
+  <ul>
+    <FragmentDemo :data="[5, 3, 2, 9 ,7 ,6]" /> 
+    <hr>
+    <TeleportDemo />
+    <hr>
+    <!-- <SuspenseDemo /> -->
+    <Suspense>
+      <template #default>
+        <AsyncComponent :timeout="3000" />
+      </template>
+      <template #fallback>
+        <loadingDemo></loadingDemo>
+      </template>
+  </Suspense>
+  </ul>
+</template>
+
+<script>
+import FragmentDemo from './fragment.vue'
+import TeleportDemo from './teleport.vue'
+import AsyncComponent from './suspense.vue'
+import loadingDemo from './loading.vue'
+export default {
+  components: {
+    FragmentDemo,
+    TeleportDemo,
+    AsyncComponent,
+    loadingDemo
+  }
+}
+</script>
+
+<style>
+
+</style>
