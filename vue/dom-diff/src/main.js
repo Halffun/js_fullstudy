800d50df105bf3207cd974ec29aefaa658a0c9c2
hufei <1521380841@qq.com>
2020-11-06 19:44:57 +0800

    dom-diff

--- vue/dom-diff/src/main.js
+++ vue/dom-diff/src/main.js
@@ -1,4 +1,6 @@
 import { createElement, render, renderDom } from './element'
+import diff from './diff'
+import patch from './patch'
 
 
 let vertualDom1 = createElement('ul', {class: 'list'}, [
@@ -6,12 +8,22 @@ let vertualDom1 = createElement('ul', {class: 'list'}, [
   createElement('li', {class: 'item'}, ['2'])
 ])
 
-let el = render(vertualDom1)
+let vertualDom2 = createElement('ul', {class: 'list-group', id: 'group'}, [
+  createElement('li', {class: 'item'}, ['1']),
+  createElement('li', {class: 'item'}, ['2']),
+  createElement('li', {class: 'item'}, ['3']),
+])
 
+
+let el = render(vertualDom1)
 renderDom(el, document.getElementById('app')) // window.app
 
+let patches = diff(vertualDom1, vertualDom2) // 补丁
+// 给元素打补丁 更新视图
+patch(el, patches)
 console.log(el);
 console.log(vertualDom1);
+console.log(patches);
 
 // vertualDom1 = <ul class="list">
 //                 <li class="item">1</li>  
