1b2e5c24ab9cf7c162c526349652c72c9b41ac49
hufei <1521380841@qq.com>
2020-11-23 22:19:45 +0800

    事件循环

--- js/面试题/看代码说输出/事件循环.js
+++ js/面试题/看代码说输出/事件循环.js
@@ -1,15 +1,15 @@
 console.log(1);
 
+setImmediate(() => {
+  console.log(4);
+  });
+
 setTimeout(() => {
 console.log(2);
 });
 
 process.nextTick(() => {
 console.log(3);
-});
-
-setImmediate(() => {
-console.log(4);
 });
 
 new Promise((resolve) => {
