4c663014040675f1a98438cb9cd6056996974383
hufei <1521380841@qq.com>
2020-12-08 21:34:47 +0800

    nodejs中的事件循环

--- js/面试题/看代码说输出/事件循环.js
+++ js/面试题/看代码说输出/事件循环.js
@@ -1,37 +1,28 @@
 console.log(1);
 
 setImmediate(() => {
-  console.log(4);
-  });
+  console.log(4);  // 无法确定
+});
 
 setTimeout(() => {
-console.log(2);
+  console.log(2); // 无法确定
 });
 
 process.nextTick(() => {
-console.log(3);
+  console.log(3);
 });
 
 new Promise((resolve) => {
-console.log(5);
-resolve();
-console.log(6);
+  console.log(5);
+  resolve();
+  console.log(6);
 }).then(() => {
-console.log(7);
+  console.log(7);
 });
 
 Promise.resolve().then(() => {
-console.log(8);
-process.nextTick(() => {
-console.log(9);
+  console.log(8);
+  process.nextTick(() => {
+    console.log(9);
+  });
 });
-});
-// 1
-// 5
-// 6
-// 3
-// 7
-// 8
-// 9
-// 2
-// 4
