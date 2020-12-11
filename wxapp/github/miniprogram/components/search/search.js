c519e241853f1f71203c5cdc25320f7873dd63ec
hufei <1521380841@qq.com>
2020-12-11 23:03:36 +0800

    github小程序

---
+++ wxapp/github/miniprogram/components/search/search.js
@@ -0,0 +1,28 @@
+// components/search/search.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  class:['cancel-class'],
+  properties: {
+    placeholder: String,
+    customClass: String
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+  // search ? 父子关系 
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    onChange: function onChange(event){
+      this.triggerEvent('search',event.detail)
+    }
+  }
+})
